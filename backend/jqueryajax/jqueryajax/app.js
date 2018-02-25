7756a8ea38947165ff513387c020f410132a6c3f
edwin <ewarandia@gmail.com>
2018-02-25 19:44:09 -0400

    fetch option

--- backend/jqueryajax/jqueryajax/app.js
+++ backend/jqueryajax/jqueryajax/app.js
@@ -1,14 +1,9 @@
 for (var i = 1; i <= 80; i++) {
-	//url= 'http://pokeapi.co/api/v2/pokemon-form/'+i,
-	
-	$.ajax({
-		url: 'http://pokeapi.co/api/v2/pokemon-form/'+i,
-		type: 'GET',
-		data: {},
-		crossDomain: true,
-		success: function(data){
-			$('.pokemons').append('<li><img src=">'+data.sprites.front_default+'"></img></li>');
-			console.log(data.sprites.front_default);
-		}
-	});	
+	fetch('http://pokeapi.co/api/v2/pokemon-form/'+i,{
+		method: 'GET'
+	}).then(function (data){
+		return data.json();
+	}).then(function(json){
+		$('.pokemons').append('<li><img src="'+json.sprites.front_default+'"></img></li>');
+	})
 }
