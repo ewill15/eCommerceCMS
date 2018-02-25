45e93976d296acff82763648365ee58e3e96fce9
edwin <ewarandia@gmail.com>
2018-02-25 17:39:20 -0400

    ajax jquery
    peticion ajax asincrona con jquery

--- backend/XMLBase/app.js
+++ backend/XMLBase/app.js
@@ -1,36 +1,35 @@
-//funcion autoejecutable
-(function($) {
-    var inputUser = $('#username');
-    var URL = 'https://api.github.com/users/';
-    inputUser.on('keypress', function(e) {
-        if (e.which === 13) {
-            var username = $(this).val().trim().toLowerCase();
-            if (username !== "") {
-                var XHR = new XMLHttpRequest();
-                var URLUser = URL + username;
-                //XHR.open('GET', URLUser); // peticion asincrona
-                XHR.open('GET', URLUser,false);//peticion sincrona
-                XHR.onreadystatechange = function() {
-                    if (XHR.readyState === 4) {
-                        var data = JSON.parse(XHR.responseText);
-                        if (data.message && data.message == 'Not Found') {
-                            alert('El usuario no existe en github');
-                        } else {
-                            $('img').attr('src', data.avatar_url);
-                            $('.name').text(data.name);
-                            $('.username').text(data.login);
-                            $('.email').text(data.email);
-                            $('.company').text(data.company);
-                            $('.location').text(data.location);
-                            $('.folowers').text(data.followers);
-                        }
-                    }
-                };
-                //envio de peticion a servidor sin parametros
-                XHR.send(null);
-            } else {
-                alert('Debe ingresar un nombre de usuario');
-            }
-        }
-    });
-})(jQuery)
+var inputUser = $('#username');
+inputUser.on('keypress',function(){
+	if(e.which === 13){
+		ajaxAsync();
+	}
+});
+
+function ajaxAsync(){
+	var URL = 'http://api.github.com/users';
+	var username = inputUser.val().trim().tolowercase();
+	if(username !== ""){
+		var Urluser = Url + username;
+		$.ajax({
+			url: Urluser,
+			type: 'GET',
+			async: true;
+			sucess: function(response){
+				var data = response;
+				if(data.message && data.message == "Not found"){
+					alert("El usuario no existe en Github");
+				}else{
+					$('img').attr('src',data.avatar_url);
+					$('.name').text(data.name);
+					$('.username').text(data.login);
+					$('.email').text(data.email);
+					$('.company').text(data.company);
+					$('.location').text(data.location);
+					$('.followers').text(data.followers);
+				}
+			}
+		});
+	}else{
+		alert('Debe ingresar un nombre de usuario');
+	}
+}
